284846c30080257552a2416f9d53d02781c728d4
Peter Divéky <info@microitem.com>
2025-02-14 22:58:54 +0100

    feat: Add DNS server management script

--- manage-dns.sh
+++ manage-dns.sh
@@ -8,91 +8,141 @@ log_message() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> ${LOG_DIR}/dns.log
 }
 
-# Funkcia pre pridanie DNS záznamu
-add_record() {
-    local name=$1
-    local type=$2
-    local content=$3
-    local ttl=${4:-3600}
+# Funkcia pre kontrolu DNS servera
+check_dns() {
+    echo "=== Kontrola DNS servera ==="
     
-    docker-compose exec -T pdns pdnsutil add-record ${EXAMPLE_DOMAIN} "$name" "$type" "$ttl" "$content"
+    # Kontrola PowerDNS
+    echo -e "\n== PowerDNS status =="
+    docker-compose exec -T pdns pdns_control status
     
-    if [ $? -eq 0 ]; then
-        log_message "Pridaný DNS záznam: $name $type $content"
-        echo "DNS záznam bol pridaný"
-    else
-        log_message "Chyba pri pridávaní DNS záznamu: $name"
-        echo "Chyba pri pridávaní záznamu"
-    fi
+    # Kontrola zón
+    echo -e "\n== DNS zóny =="
+    docker-compose exec -T pdns pdns_control list-zones
+    
+    # Test resolvovania
+    echo -e "\n== Test DNS =="
+    for domain in ${EXAMPLE_DOMAIN} www.${EXAMPLE_DOMAIN}; do
+        echo "Test pre $domain:"
+        dig @localhost $domain +short
+    done
+    
+    # Štatistiky
+    echo -e "\n== DNS štatistiky =="
+    docker-compose exec -T pdns pdns_control show "*"
 }
 
-# Funkcia pre odstránenie DNS záznamu
-remove_record() {
-    local name=$1
-    local type=$2
+# Funkcia pre správu zón
+manage_zone() {
+    local action=$1
+    local domain=$2
     
-    docker-compose exec -T pdns pdnsutil delete-rrset ${EXAMPLE_DOMAIN} "$name" "$type"
-    
-    if [ $? -eq 0 ]; then
-        log_message "Odstránený DNS záznam: $name $type"
-        echo "DNS záznam bol odstránený"
-    else
-        log_message "Chyba pri odstraňovaní DNS záznamu: $name"
-        echo "Chyba pri odstraňovaní záznamu"
-    fi
+    case $action in
+        add)
+            # Vytvorenie zóny
+            docker-compose exec -T pdns pdnsutil create-zone "$domain"
+            docker-compose exec -T pdns pdnsutil set-kind "$domain" NATIVE
+            
+            # Pridanie základných záznamov
+            docker-compose exec -T pdns pdnsutil add-record "$domain" "" A "300" "${DEFAULT_IP}"
+            docker-compose exec -T pdns pdnsutil add-record "$domain" "www" A "300" "${DEFAULT_IP}"
+            
+            log_message "Pridaná zóna: ${domain}"
+            echo "Zóna bola pridaná"
+            ;;
+            
+        remove)
+            docker-compose exec -T pdns pdnsutil delete-zone "$domain"
+            
+            log_message "Odstránená zóna: ${domain}"
+            echo "Zóna bola odstránená"
+            ;;
+            
+        list)
+            echo "Aktívne zóny:"
+            docker-compose exec -T pdns pdns_control list-zones
+            ;;
+            
+        export)
+            local export_file="${BACKUP_DIR}/zone_${domain}_$(date +%Y%m%d).txt"
+            docker-compose exec -T pdns pdns_control list-zone "$domain" > "$export_file"
+            
+            echo "Zóna bola exportovaná do: $export_file"
+            ;;
+            
+        import)
+            local import_file=$3
+            if [ ! -f "$import_file" ]; then
+                echo "Súbor neexistuje: $import_file"
+                exit 1
+            fi
+            
+            docker-compose exec -T pdns pdnsutil load-zone "$domain" "$import_file"
+            
+            log_message "Importovaná zóna: ${domain}"
+            echo "Zóna bola importovaná"
+            ;;
+            
+        *)
+            echo "Neznáma akcia: $action"
+            exit 1
+            ;;
+    esac
 }
 
-# Funkcia pre výpis DNS záznamov
-list_records() {
-    echo "Zoznam DNS záznamov:"
-    docker-compose exec -T pdns pdnsutil list-zone ${EXAMPLE_DOMAIN}
-}
-
-# Funkcia pre export zóny
-export_zone() {
-    local output_file="${BACKUP_DIR}/zone_${EXAMPLE_DOMAIN}_$(date +%Y%m%d).txt"
-    docker-compose exec -T pdns pdnsutil list-zone ${EXAMPLE_DOMAIN} > "$output_file"
+# Funkcia pre správu záznamov
+manage_record() {
+    local action=$1
+    local domain=$2
+    local name=$3
+    local type=$4
+    local content=$5
+    local ttl=${6:-300}
     
-    if [ $? -eq 0 ]; then
-        log_message "Zóna exportovaná do: $output_file"
-        echo "Zóna bola exportovaná do: $output_file"
-    else
-        log_message "Chyba pri exporte zóny"
-        echo "Chyba pri exporte zóny"
-    fi
-}
-
-# Funkcia pre kontrolu zóny
-check_zone() {
-    echo "Kontrola zóny ${EXAMPLE_DOMAIN}:"
-    docker-compose exec -T pdns pdnsutil check-zone ${EXAMPLE_DOMAIN}
+    case $action in
+        add)
+            docker-compose exec -T pdns pdnsutil add-record "$domain" "$name" "$type" "$ttl" "$content"
+            
+            log_message "Pridaný záznam: ${name}.${domain} ${type} ${content}"
+            echo "Záznam bol pridaný"
+            ;;
+            
+        remove)
+            docker-compose exec -T pdns pdnsutil delete-rrset "$domain" "$name" "$type"
+            
+            log_message "Odstránený záznam: ${name}.${domain} ${type}"
+            echo "Záznam bol odstránený"
+            ;;
+            
+        list)
+            echo "Záznamy pre zónu $domain:"
+            docker-compose exec -T pdns pdnsutil list-zone "$domain"
+            ;;
+            
+        *)
+            echo "Neznáma akcia: $action"
+            exit 1
+            ;;
+    esac
 }
 
 # Spracovanie parametrov
 case "$1" in
-    add)
-        add_record "$2" "$3" "$4" "$5"
-        ;;
-    remove)
-        remove_record "$2" "$3"
-        ;;
-    list)
-        list_records
-        ;;
-    export)
-        export_zone
-        ;;
     check)
-        check_zone
+        check_dns
+        ;;
+    zone)
+        manage_zone "$2" "$3" "$4"
+        ;;
+    record)
+        manage_record "$2" "$3" "$4" "$5" "$6" "$7"
         ;;
     *)
-        echo "Použitie: $0 {add|remove|list|export|check} [parametre]"
+        echo "Použitie: $0 {check|zone|record} [parametre]"
         echo "Príklady:"
-        echo "  $0 add subdomain A 192.168.1.1 [ttl]"
-        echo "  $0 remove subdomain A"
-        echo "  $0 list"
-        echo "  $0 export"
         echo "  $0 check"
+        echo "  $0 zone {add|remove|list|export|import} domain [import_file]"
+        echo "  $0 record {add|remove|list} domain name type [content] [ttl]"
         exit 1
 esac
 
